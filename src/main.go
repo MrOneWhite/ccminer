2c1a9ac9f4e635d6f0b1f8104b91a176cbcad48b
Mr. One White <101678370+MrOneWhite@users.noreply.github.com>
2021-05-25 05:29:44 +0800

    Init repo. Set up JSON RPC Client

--- src/main.go
+++ src/main.go
@@ -1,34 +1,75 @@
 package main
 
 import (
-	"gobtcminer/client"
-	"gobtcminer/config"
-	"gobtcminer/logger"
-	"gobtcminer/mining"
-	"time"
+	"encoding/json"
+	"github.com/btcsuite/btcrpcclient"
+	"io/ioutil"
+	"log"
 )
 
-var monitor logger.Logger
+type Config struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Account  string `json:"account"`
+}
+
+func readconf() (conf Config) {
+	content, err := ioutil.ReadFile("../config.json")
+	if err != nil {
+		log.Fatalf("Error:", err)
+	}
+	err = json.Unmarshal(content, &conf)
+	if err != nil {
+		log.Fatalf("Error:", err)
+	}
+	return
+}
+func VerifyAccount(client *btcrpcclient.Client, name string) bool {
+	adr, err := client.GetAccountAddress(name)
+	if err != nil {
+		log.Printf("Error getting account address %s", name)
+		return false
+	} else {
+		wal, err := client.ValidateAddress(adr)
+		if err != nil {
+			log.Printf("Error validating account address")
+			return false
+		} else if !wal.IsValid {
+			log.Printf("Invalid account address")
+			return false
+		}
+		log.Printf("Account: %s, Address: %s, PubKey: %s\n", name, adr, wal.PubKey)
+	}
+	return true
+}
+func ListAccounts(client *btcrpcclient.Client) {
+	accounts, err := client.ListAccounts()
+	if err != nil {
+		log.Fatalf("Error listing accounts: %v", err)
+	}
+	for label, amount := range accounts {
+		log.Println("Account %s with %s", label, amount)
+	}
+	log.Fatalf("Indicates the right account in config.json then try again.")
+}
 
 func main() {
-
 	// Read and parse the configuration file
-	conf := config.ReadConf("config.json")
-	monitor = logger.NewLogger(conf.Log)
-	diff, err := client.GetDifficulty(conf.User, conf.Password, conf.Host)
+	conf := readconf()
+	// Create new client instance
+	client, err := btcrpcclient.New(&btcrpcclient.ConnConfig{
+		HTTPPostMode: true,
+		DisableTLS:   true,
+		Host:         conf.Host,
+		User:         conf.User,
+		Pass:         conf.Password,
+	}, nil)
 	if err != nil {
-		monitor.Print("info", "Error getting difficulty: "+err.Error())
+		log.Fatalf("error creating new btc client: %v", err)
 	}
-	dispatcher := mining.NewDispatcher(monitor)
-	dispatcher.Run()
-	//Add Chunks on a regular basis
-	for {
-		//Get a new Chunk and split it accordingly to the machin settings
-		for _, chunk := range mining.NewChunkList(2, uint32(time.Now().Unix()), diff) {
-			if len(dispatcher.ChunkQueueIn) < cap(dispatcher.ChunkQueueIn) {
-				dispatcher.ChunkQueueIn <- chunk
-			}
-		}
-		time.Sleep(10 * time.Millisecond)
+	// Verifying Account
+	if !VerifyAccount(client, conf.Account) {
+		ListAccounts(client)
 	}
 }
