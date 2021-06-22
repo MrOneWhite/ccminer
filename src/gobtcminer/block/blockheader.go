e589d83facb969e62f5a8b767bd52a4317db24a4
Mr. One White <101678370+MrOneWhite@users.noreply.github.com>
2021-06-22 21:13:11 +0800

    Added blockheader validation

--- src/gobtcminer/block/blockheader.go
+++ src/gobtcminer/block/blockheader.go
@@ -7,94 +7,45 @@ For difficulty history, see: http://www.coindesk.com/data/bitcoin-mining-difficu
 
 package block
 
-import (
-	"crypto/sha256"
-	"encoding/hex"
-	"errors"
-	"math/rand"
-	"strconv"
-	"time"
-)
+import "time"
 
-const bitcoinCreationDate uint32 = 1230940800
+//Macros
+const BITCOIN_CREATION_DATE uint32 = 1230940800
+const INITIAL_DIFFICULTY float64 = 1
 
-//Header structure
-type Header struct {
-	Version       byte   //Block Version Number. Decimal format. 4 bytes Little Endian format originally.
-	HashPrevBlock string //256bits Hash of the previous block header
-	HashMerkRoot  string //256bits Hash on all of the transactions in the block
-	Time          uint32 //Timestamp - Epoch time
-	Bits          uint32 //Current target calculated with difficulty. Temporary string representation for testing.
-	Nonce         uint32 //32Bits number - iterator
+type BlockHeader struct {
+	Version       uint32  //Block Version Number
+	HashPrevBlock string  //256bits Hash of the previous block header
+	HashMerkRoot  string  //256bits Hash on all of the transactions in the block
+	Time          uint32  //Timestamp - Epoch time
+	Bits          float64 //Current target in compact format
+	Nonce         uint32  //32Bits number - iterator
 }
 
-// Validate a block header
-// Difficulty is not checked since we might need to check older block. Nonce neither as it starts at 0.
-func (block *Header) Validate() error {
-	err := errors.New("invalid version")
-	if block.Version < 1 || block.Version > 3 {
-		return err
+//Validate the syntax of each field. Difficulty is not checked since we might need to check older block. Nonce either since it starts at 0.
+func Validate(block BlockHeader) bool {
+	valid_version := false
+	version_list := [3]uint32{02000000, 03000000, 04000000}
+	for _, version := range version_list {
+		if block.Version == version {
+			valid_version = true
+			break
+		}
+	}
+	if valid_version == false {
+		return false
 	}
 
 	if len(block.HashPrevBlock) > 32 || len(block.HashMerkRoot) > 32 {
-		return err
+		return false
 	}
 
 	//1230940800 is 3th Jan 2009 - First Version of Bitcoin
-	if block.Time < bitcoinCreationDate || block.Time > uint32(time.Now().Unix()) {
-		return err
+	if block.Time < BITCOIN_CREATION_DATE || int64(block.Time) > time.Now().Unix() {
+		return false
 	}
-	return nil
-}
-
-//Return the hex string of a given block header.
-func (block *Header) hex() string {
-	hexVersion := strconv.FormatInt(int64(block.Version), 16) //Little Endian format already, we keep it that way.
-	switch length := len(hexVersion); length {
-	case 1:
-		hexVersion = "0" + hexVersion + "000000"
-	case 2:
-		hexVersion = hexVersion + "000000"
+	if block.Bits < INITIAL_DIFFICULTY {
+		return false
 	}
-	hexTime := strconv.FormatInt(int64(block.Time), 16)
-	hexNonce := strconv.FormatInt(int64(block.Nonce), 16)
-	hexBits := strconv.FormatInt(int64(block.Bits), 16)
-	return hexVersion + block.HashPrevBlock + block.HashMerkRoot + hexTime + hexBits + hexNonce
-}
-
-//  Common functions
-//
-
-//MakeSemiRandomBlockHeader makes a semi-random block header. Uses pre-defined time and version. Faster to generate than fully random blockheader.
-func MakeSemiRandomBlockHeader(version byte, currentTime uint32) Header {
-	hashprevblock := randStringBytes(64)
-	hashmerkroot := randStringBytes(64)
-	rand.Seed(int64(currentTime))
-	nonce := rand.Uint32()
-	bits := rand.Uint32()
-	return Header{Version: version, HashPrevBlock: hashprevblock, HashMerkRoot: hashmerkroot, Bits: bits, Time: currentTime, Nonce: nonce}
-}
-
-//Generate Hex string-representated number of n characters
-func randStringBytes(n int) string {
-	const letterBytes = "abcdef0123456789"
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
-}
-
-//Return a Sha256 Hash of given data
-func hash256(data []byte) []byte {
-	hash := sha256.New()
-	hash.Write(data)
-	return hash.Sum(nil)
-}
-
-//Doublesha256BlockHeader returns a string representation of doubled-hashed block header
-func Doublesha256BlockHeader(bh Header) string {
-	data := []byte(bh.hex())
-	hash := hash256(hash256(data))
-	return hex.EncodeToString(hash)
+	return true
 }
