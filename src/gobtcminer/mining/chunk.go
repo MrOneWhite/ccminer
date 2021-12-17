55544d45ed7aeab28fe0b4e6ba1e335b30401fad
Mr. One White <101678370+MrOneWhite@users.noreply.github.com>
2021-12-17 10:25:24 +0800

    Starting redesigning the mining process. Chunk object aside

--- src/gobtcminer/mining/chunk.go
+++ src/gobtcminer/mining/chunk.go
@@ -10,39 +10,17 @@ import "gobtcminer/block"
 
 //Chunk entity defined by a block and a target. Nonce is here for checking if the chunk is valid
 type Chunk struct {
-	Block      block.Header
-	Target     string
-	StartNonce uint32
-	EndNonce   uint32
-	Valid      bool
+	Block  block.BlockHeader
+	Target string
+	Nonce  uint32
 }
 
-//NewChunkList We are splitting a BlockHeader to spread the mining between the different goroutines
-func NewChunkList(version byte, epoch uint32, difficulty float64) []Chunk {
-	newBlock := block.MakeSemiRandomBlockHeader(version, epoch)
-	target := Gettarget(difficulty, newBlock.Bits)
-	chunklist := make([]Chunk, Psize)
-	for i := 0; i < Psize; i++ {
-		// Convert values to match MaxNonce type
-		poolsize := uint32(Psize)
-		iterator := uint32(i)
-		startvalue := iterator * (MaxNonce / poolsize)
-		if i > 0 {
-			startvalue++
-		}
-		endvalue := (iterator + 1) * (MaxNonce / poolsize)
-		chunklist[i] = newChunk(newBlock, target, startvalue, endvalue)
-	}
-	return chunklist
-}
-
-// Returns a new object Chunk
-func newChunk(blockheader block.Header, target string, startnonce, endnonce uint32) Chunk {
+// Creating Chunk 'job'
+func NewChunk(version byte, epoch uint32, difficulty float64) Chunk {
+	new_block := block.MakeSemiRandom_BlockHeader(version, epoch)
+	target := Gettarget(difficulty, new_block.Bits)
 	return Chunk{
-		Block:      blockheader,
-		Target:     target,
-		StartNonce: startnonce,
-		EndNonce:   endnonce,
-		Valid:      false,
-	}
+		Block:  new_block,
+		Target: target,
+		Nonce:  0}
 }
--- src/gobtcminer/mining/parameters.go
+++ src/gobtcminer/mining/parameters.go
@@ -6,15 +6,18 @@ Description: Define mining parameters specific to Bitcoin BP023
 
 package mining
 
-import (
-	"strconv"
-	"strings"
-)
+import "strconv"
 
-//Gettarget TODO Calculate the right target depending on the difficulty. This one is totally made up for testing purpose.
+//TODO Calculate the right target depending on the difficulty. This one is totally made up for testing purpose.
 func Gettarget(difficulty float64, bits uint32) string {
 	const padding int = 17
-	target := strings.Repeat("0", padding) + strconv.Itoa(int(uint32(difficulty)*bits))
-	suffix := strings.Repeat("f", 64-len(target))
-	return target + suffix
+	var target = ""
+	for i := 0; i < padding; i++ {
+		target = target + "0"
+	}
+	target = target + strconv.Itoa(int(uint32(difficulty)*bits))
+	for i := len(target); i < 64; i++ {
+		target = target + "f"
+	}
+	return target
 }
