c542da1b95d9c8f973cdbe698200274d839fceec
Mr. One White <101678370+MrOneWhite@users.noreply.github.com>
2022-01-10 07:58:07 +0800

    Added logger in charge of keeping track of the mining pool performance

--- src/gobtcminer/logger/logger.go
+++ src/gobtcminer/logger/logger.go
@@ -10,50 +10,32 @@ import (
 	"gobtcminer/config"
 	"log"
 	"sync"
-	"time"
 )
 
 //Logger object.
 type Logger struct {
-	Activated  bool       //Can be activatede, or not.
-	Level      string     //Levels: 'debug' and 'info'
-	File       string     //Filename for storing the output
-	HashCount  uint32     //Global count of hashes executed so far
-	BlockCount uint32     //Global count of hashes executed so far
-	mux        sync.Mutex //Mutex for avoiding concurrency on increasing HashCount
-	BeginTime  time.Time  //Used for calculating the compute time for benchmarking
+	Activated bool       //Can be activatede, or not.
+	Level     string     //Levels: 'debug' and 'info'
+	File      string     //Filename for storing the output
+	HashCount uint32     //Global count of hashes executed so far
+	mux       sync.Mutex //Mutex for avoiding concurrency on increasing HashCount
 }
 
-//NewLogger Constructor function
-func NewLogger(logger config.JSONLogger) Logger {
+//Constructor function
+func NewLogger(logger config.JsonLogger) Logger {
 	return Logger{
 		Activated: logger.Activated,
 		Level:     logger.Level,
-		File:      logger.File,
-	}
+		File:      logger.File}
 }
 
-//Print simply logs
+//Log
 func (logger *Logger) Print(level string, output string) {
 	if logger.Activated {
-		if logger.Level == level {
+		if logger.Level == "info" {
 			log.Println(output)
-		} else if logger.Level == "debug" && level == "info" {
+		} else if logger.Level == "debug" {
 			log.Println(output)
 		}
 	}
-}
-
-//IncrementHashCount counts the number of hash executed regularly. Use of a mutex to avoid race condition.
-func (logger *Logger) IncrementHashCount(count uint32) {
-	logger.mux.Lock()
-	defer logger.mux.Unlock()
-	logger.HashCount += count
-}
-
-//IncrementBlockCount increments the number of succesfuly mined block. Use of a mutex to avoid race condition.
-func (logger *Logger) IncrementBlockCount() {
-	logger.mux.Lock()
-	defer logger.mux.Unlock()
-	logger.BlockCount++
 }
