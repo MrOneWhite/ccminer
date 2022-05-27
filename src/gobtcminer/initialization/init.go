37531c31d735481155d2e70b7ca802c87e39850a
Mr. One White <101678370+MrOneWhite@users.noreply.github.com>
2022-05-27 09:35:26 +0800

    Added initialization to analyze machine performance, in order to make proper chunk split between the minesr

--- src/gobtcminer/initialization/init.go
+++ src/gobtcminer/initialization/init.go
@@ -2,8 +2,6 @@
 Author: Mathieu Mailhos
 Filename: init.go
 Description: Determin the performances of the machin. Gives the machin 5 seconds to mine as many blocks as possible on a given random chunk. This will allow a more efficient split for the block header between the different miners, using this pre-defined value
-
-DEPRECATED - Use of a Timeout per miner instead of this approximation
 */
 
 package initialization
@@ -18,27 +16,26 @@ import (
 	"time"
 )
 
-var globalCounter counter
+var counter Counter
 
-type counter struct {
+type Counter struct {
 	HashCount uint32
 	mux       sync.Mutex
 }
 
-//HashCountSpan Counter big enough to avoid mutex bottleneck but small enough to be as precise as possible
-const HashCountSpan = 5000
+const HASHCOUNT_SPAN = 5000 //Counter big enough to avoid mutex bottleneck but small enough to be as precise as possible
 
 //Determine the number of double sha256 hash the machin is able to run per second. This function will allow to better split the Chunks with a pre-defined value.
-func init() uint32 {
+func Init() uint32 {
 	log.Println("Starting initialization. Determining machin performances... (5s)")
-	bh := block.MakeSemiRandomBlockHeader(0, 0)
+	bh := block.MakeSemiRandom_BlockHeader(0, 0)
 	timeout := make(chan bool, 1)
 	go func() {
 		time.Sleep(5 * time.Second)
 		timeout <- true
 	}()
 	for i := 0; i < mining.Psize; i++ {
-		go noendMining(bh, timeout)
+		go noend_mining(bh, timeout)
 	}
 	for {
 		select {
@@ -47,8 +44,8 @@ func init() uint32 {
 				timeout <- true
 			}
 			fmt.Println("")
-			total := globalCounter.HashCount / 5 / uint32(mining.Psize)
-			log.Println("Done initializing: " + strconv.Itoa(int(total)) + " operations per seconds per thread")
+			total := counter.HashCount / 5
+			log.Println("Done initializing: " + strconv.Itoa(int(total)) + " operations per seconds.")
 			return total
 		case <-time.After(time.Second * 1):
 			fmt.Print(". ")
@@ -57,16 +54,16 @@ func init() uint32 {
 }
 
 //Miner mining a fake random Chunk while not receiving a timeout
-func noendMining(bh block.Header, timeout chan bool) {
+func noend_mining(bh block.BlockHeader, timeout chan bool) {
 	for count, nonce := uint32(0), uint32(0); true; nonce, count = nonce+1, count+1 {
 		select {
 		case <-timeout:
 			return
 		default:
 			bh.Nonce = nonce
-			block.Doublesha256BlockHeader(bh)
+			block.Doublesha256_BlockHeader(bh)
 			if count == HASHCOUNT_SPAN {
-				globalCounter.HashCount += HashCountSpan
+				counter.HashCount += HASHCOUNT_SPAN
 				count = 0
 			}
 		}
